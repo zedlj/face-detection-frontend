aa0d82eeb2e09497b61063819a43a606c08c7421
zedlj <zoelauren-jones@hotmail.co.uk>
2020-05-15 10:38:08 +0100

    deploy

--- src/App.js
+++ src/App.js
@@ -25,16 +25,16 @@ const particlesOptions = {
 
 const initialState = {
    input: '',
-        imageUrl: '',
-        box: {},
-        route: 'signin',
-        isSignedIn: false,
-        user: {
-          id: '',
-          name: '',
-          email: '',
-          entries: 0,
-          joined: new Date(),
+   imageUrl: '',
+   box: {},
+   route: 'signin',
+   isSignedIn: false,
+   user: {
+      id: '',
+      name: '',
+      email: '',
+      entries: 0,
+      joined: new Date(),
         }
 }
 
