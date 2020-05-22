7f68f56fb4002b20dfdc58569df47ee58f87761a
zedlj <zoelauren-jones@hotmail.co.uk>
2020-05-22 20:06:49 +0100

    face2 data

--- src/App.js
+++ src/App.js
@@ -58,7 +58,8 @@ class App extends Component {
 
   calculateFaceLocation = (data) => {
     console.log('FULL API DATA:', data)
-    console.log('BOX API DATA', data.outputs[0].data.regions[0].region_info.bounding_box)
+    console.log('BOX1 API DATA', data.outputs[0].data.regions[0].region_info.bounding_box)
+    console.log('BOX2 API DATA', data.outputs[0].data.regions[1].region_info.bounding_box)
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputImage');
     const width = Number(image.width);
